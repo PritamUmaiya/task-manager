4a280d0cae37aabd87108937e664a265f2567b50
Pritam Jaiswal <hello.pritamjaiswal@gmail.com>
2024-08-15 02:26:21 +0000

    adding backend authentication

--- app.py
+++ app.py
@@ -43,7 +43,41 @@ def register():
     """Register the user"""
 
     if request.method == "POST":
-        ...
+        username = request.form['username']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+
+        # Make sure user has provied all the required details
+        if not username or not password or not confirm_password:
+            flash("Please enter your details", "info")
+            return redirect(request.referrer)
+
+        # Validate username
+        usernames = db.execute("SELECT id FROM users WHERE username = ?", username)
+
+        if len(usernames) > 0:
+            flash("Username already exists!", "info")
+            return redirect(request.referrer)
+
+        # Validate passwords
+        if password != confirm_password:
+            flash("Password did not matched!", "info")
+            return redirect(request.referrer)
+
+        # Hash the password
+        password = generate_password_hash(password)
+
+        # Add the user to database
+        db.execute("INSERT INTO users(username, password) VALUES(?, ?)", username, password)
+
+        users = db.execute("SELECT id FROM users WHERE username = ?", username)
+
+        # Clear session and session for current user
+        session.clear()
+        session['user_id'] = users[0]['id']
+
+        flash("Account created successfully!", "success")
+        return redirect("/")
 
     else:
         return render_template("register.html")
@@ -54,8 +88,26 @@ def login():
     """Login the user"""
 
     if request.method == "POST":
-        ...
-    
+        username = request.form['username']
+        password = request.form['password']
+
+        if not username or not password:
+            flash("Please enter your details!", "info")
+            return redirect(request.referrer)
+
+        # Check if username exists and password is correct
+        users = db.execute("SELECT id, password FROM users WHERE username = ?", username)
+
+        if not len(users) == 1 or not check_password_hash(users[0]['password'], password):
+            flash("Invalid username/password!", "danger")
+            return redirect(request.referrer)
+
+        # Add the user to session
+        session.clear()
+        session['user_id'] = users[0][id]
+
+        return redirect("/")
+
     else:
         return render_template("login.html")
 
